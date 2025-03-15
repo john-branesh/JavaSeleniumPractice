001a87e6aeb41275bee4ad44c4615574bf5fbc98
john branesh r <johragup@amazon.com>
2025-03-15 15:33:36 +0530

    another exercise

---
+++ src/Practice/ScreenShots.java
@@ -0,0 +1,38 @@
+package Practice;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.function.Function;
+
+public class ScreenShots {
+    public static void main (String[] args) throws IOException {
+        System.setProperty("webdriver.gecko.driver", "/Users/johragup/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.amazon.com/");
+
+        //fluent wait
+        Wait<WebDriver> fluentWait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(2)).pollingEvery(Duration.ofSeconds(2))
+                .ignoring(NoSuchElementException.class);
+        WebElement ScreenImage = fluentWait.until(new Function<WebDriver, WebElement>(){
+            public WebElement apply(WebDriver driver){
+                return driver.findElement(By.xpath("//*[@id=\'nav-logo-sprites\']"));
+            }
+        });
+
+        //Take screenshot
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        //convert this ss to file type, also this is the place from where image will be copied.
+        File Image = screenshot.getScreenshotAs( OutputType.FILE);
+        //Mention the destination where ss should be saved
+        File location = new File( "/Users/johragup/Downloads/sample.png");
+        //copy paste the file from selenium to local host
+        FileHandler.copy(Image, location);
+
+    }}
--- src/Selenium_2/hello_world.java
+++
@@ -1,7 +0,0 @@
-package Selenium_2;
-
-public class hello_world {
-    public static void main (String[] args){
-        System.out.println("Hello, world");    // prints output to console
-    }
-}
